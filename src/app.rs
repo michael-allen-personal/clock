b640c0483541d9a3f3891005dc758a1d95e67511
Michael Allen <mtallen2014@gmail.com>
2023-12-13 00:01:58 -0600

    Got basic counters to set time, and countdown working

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,11 @@
+use std::time::Instant;
+
 pub struct ClockApp {
-    hour: i32,
-    min: i32,
-    sec: i32,
+    hour: i64,
+    min: i64,
+    sec: i64,
+    countdown_active: bool,
+    start_time: Instant,
 }
 
 impl Default for ClockApp {
@@ -10,6 +14,8 @@ impl Default for ClockApp {
             hour: 0,
             min: 0,
             sec: 0,
+            countdown_active: false,
+            start_time: Instant::now(),
         }
     }
 }
@@ -36,13 +42,36 @@ impl ClockApp {
 impl eframe::App for ClockApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello World!");
-            self.hms_input(ui)
+            if self.countdown_active {
+                let elapsed = self.start_time.elapsed().as_secs() as i64;
+                let total_seconds = self.hour * 60 * 60 + self.min * 60 + self.sec;
+                let remaining_sec = total_seconds - elapsed;
+                if remaining_sec < 0 {
+                    self.countdown_active = false;
+                    return;
+                }
+
+                // Convert remaining_sec to hours, minutes, and seconds
+                let hours = remaining_sec / 3600;
+                let minutes = (remaining_sec % 3600) / 60;
+                let seconds = remaining_sec % 60;
+
+                ui.label(format!("{:02}:{:02}:{:02}", hours, minutes, seconds));
+                if ui.button("Stop").clicked() {
+                    self.countdown_active = false;
+                }
+            } else {
+                self.hms_input(ui);
+                if ui.button("Start").clicked() {
+                    self.countdown_active = true;
+                    self.start_time = Instant::now();
+                }
+            }
         });
     }
 }
 
-fn ui_counter(ui: &mut egui::Ui, counter: &mut i32, label_text: Option<String>) {
+fn ui_counter(ui: &mut egui::Ui, counter: &mut i64, label_text: Option<String>) {
     // Put the buttons and label on the same row:
     ui.horizontal(|ui| {
         if label_text.is_some() {
